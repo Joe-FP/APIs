3810bb07caee6c5eda9e3098d9c0ec368482cbe0
JoeFPollock <josephfpollock@gmail.com>
2018-08-03 17:49:00 +0100

    Multiple responses from the API - trying to add timeout

--- public/app2.js
+++ public/app2.js
@@ -1,70 +1,52 @@
-let request;
 let mainMap;
-let arrURL = [];
 
 const getRandomInt = function(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const createIPData = function(){
-  // Create IP addresses for a given region
-  for (let i = 0; i < 10; i++) {
-    // Switzerland 31.130.224.0 - 31.130.239.255
-    let num1 = getRandomInt(0, 255);
+const generateURLs = function(numURLs){
+  let arrURL = [];
+  for (let i = 0; i < numURLs; i++) {
+    let num1 = getRandomInt(3, 216);
     let num2 = getRandomInt(0, 255);
     let num3 = getRandomInt(0, 255);
+    let num4 = getRandomInt(0, 255);
     // url returns json data for IP
-    let url = `https://ipapi.co/31.${num1}.${num2}.${num3}/json/`;
+    let url = `https://ipapi.co/${num1}.${num2}.${num3}.${num4}/json/`;
     arrURL[i] = url;
   };
-  makeRequests();
+  requestIPData(arrURL);
 };
 
-const makeRequests = function(){
-  // arrURL.forEach(function(url) {
-    //create a new XMLHttpRequest object
-    // let request = new XMLHttpRequest();
-    let nRequest = [];
-    for (var i=0; i<10; i++){
+const requestIPData = function(arrURL){
+  let arrRequests = [];
+    for (var i = 0; i < arrURL.length; i++){
        (function(i) {
-          nRequest[i] = new XMLHttpRequest();
-          nRequest[i].open("GET", arrURL[i], true);
-          console.log(arrURL[i]);
-          nRequest[i].onreadystatechange = function (oEvent) {
-             if (nRequest[i].readyState === 4) {
-                if (nRequest[i].status === 200) {
-                  console.log(nRequest[i].responseText);
-                  const jsonString = this.responseText;
-                  let ipData = JSON.parse(jsonString);
-                  mainMap.addMarker([ipData.latitude, ipData.longitude])
-                  // alert(nRequest[i].responseText);
-                } else {
-                  // console.log("Error", nRequest[i].statusText);
-                }
-             }
+          arrRequests[i] = new XMLHttpRequest();
+          arrRequests[i].open("GET", arrURL[i], true);
+          arrRequests[i].onreadystatechange = function (event) {
+            if (arrRequests[i].readyState !== 4 && arrRequests[i].status === 200) return;
+            if (arrRequests[i].status === 200) {
+              const jsonString = this.responseText;
+              markIPLocation(jsonString);
+            } else {
+              console.log("Error", arrRequests[i].statusText);
+            };
           };
-          nRequest[i].send(null);
+          arrRequests[i].send(null);
        })(i);
     }
-
-    // request.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //       const jsonString = this.responseText;
-    //       let ipData = JSON.parse(jsonString);
-    //       mainMap.addMarker([ipData.latitude, ipData.longitude])
-    //     };
-    // };
-    // request.open("GET", url, true)
-    // request.send();
-  // });
-  // request.addEventListener('load', requestComplete);
 };
 
+const markIPLocation = function(jsonString){
+  let ipData = JSON.parse(jsonString);
+  mainMap.addMarker([ipData.latitude, ipData.longitude]);
+}
 
 const app = function(){
   // Initialise MapWrapper(containerID, coords, zoom)
-  mainMap = new MapWrapper('main-map', [0, 0], 5);
-  createIPData();
+  mainMap = new MapWrapper('main-map', [0, 0], 2);
+  generateURLs(5);
 }
 
 window.addEventListener('DOMContentLoaded', app);
